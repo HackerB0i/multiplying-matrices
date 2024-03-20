495173d794760ada98d388226963e99ef2f415f9
skonk <anntvfamily@gmail.com>
2024-03-20 15:28:35 -0700

    comments

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ Name: 2x2 Matrix Multiplication
 Description: multiplies two matrixes, another school related script
 """
 
-matrix1 = []
+# set up the arrays
+matrix1 = [] 
 matrix2 = []
 
-i = 0
+# input for matrix 1
 for row in range(2):
     for column in range(2):
         matrix1.append(int(input(f"a-{row}{column}: ")))
 
-
+# input for matrix 2
 for row in range(2):
     for column in range(2):
         matrix2.append(int(input(f"b-{row}{column}: ")))
-                         
-print(f"[{(matrix1[0] * matrix2[0]) + (matrix1[1] * matrix2[2])} {(matrix1[0] * matrix2[1]) + (matrix1[1] * matrix2[3])}]")
 
+# print results                         
+print(f"[{(matrix1[0] * matrix2[0]) + (matrix1[1] * matrix2[2])} {(matrix1[0] * matrix2[1]) + (matrix1[1] * matrix2[3])}]")
 print(f"[{(matrix1[2] * matrix2[0]) + (matrix1[3] * matrix2[2])} {(matrix1[2] * matrix2[1]) + (matrix1[3] * matrix2[3])}]")
